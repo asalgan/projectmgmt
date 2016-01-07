c1fb19bc69df37af34bb05fd44a64b8573d72365
Alan Salganik <aasalganik1@gmail.com>
2016-01-07 12:16:07 -0600

    lots of restyling changes

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -26,6 +26,7 @@ class OrganizationsController < ApplicationController
       @organization = Organization.find(params[:id])
       @uncategorized_brain_dump = @organization.brain_dumps.where(brain_dump_category_id: nil).reverse
       @brain_dump_categories = @organization.brain_dump_categories.includes(:brain_dumps => [ { :brain_dump_notes => :user }, :users ])
+      @brain_dump_items = @brain_dump_categories.collect(&:brain_dumps).flatten
     end
 
 
