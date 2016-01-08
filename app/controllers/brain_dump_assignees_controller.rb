125ba3b42027b080c9b4653ea9eaa73ea564dd19
Alan Salganik <aasalganik1@gmail.com>
2016-01-08 17:26:03 -0600

    drag and drop assignments

---
+++ app/controllers/brain_dump_assignees_controller.rb
@@ -0,0 +1,9 @@
+class BrainDumpAassigneesController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,6 @@ Rails.application.routes.draw do
 
   resources :brain_dump_notes
 
+  resources :brain_dump_assignees
+
 end
