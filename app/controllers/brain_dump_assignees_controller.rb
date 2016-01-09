4d32ca077226e6cf814af9378253e617bc78c769
Alan Salganik <aasalganik1@gmail.com>
2016-01-08 19:27:18 -0600

    can successfully asssign tasks

--- app/controllers/brain_dump_assignees_controller.rb
+++ app/controllers/brain_dump_assignees_controller.rb
@@ -1,9 +1,11 @@
-class BrainDumpAassigneesController < ApplicationController
+class BrainDumpAssigneesController < ApplicationController
 
   def new
   end
 
   def create
+    @brain_dump_assignment = BrainDumpAssignee.create(brain_dump_id: params[:brain_dump_id], user_id: params[:user_id])
+    render nothing: true
   end
 
 end
